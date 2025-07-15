9b57901bc9083bd83533425dddf1fa566649877c
h.asefi <assefi.hossein@gmail.com>
2025-07-15 11:38:12 +0330

    update UrlValidatorHelper javadoc

--- java/demo/src/main/java/com/h/asefi/demo/common/helper/UrlValidatorHelper.java
+++ java/demo/src/main/java/com/h/asefi/demo/common/helper/UrlValidatorHelper.java
@@ -7,7 +7,18 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+/**
+ * Utility class for validating URL strings.
+ * <p>
+ * Provides methods to check if a URL is valid and to throw a custom exception if the URL is invalid.
+ */
 public class UrlValidatorHelper {
+    /**
+     * Checks if the provided string is a valid URL.
+     *
+     * @param url the URL string to validate
+     * @return {@code true} if the URL is valid, {@code false} otherwise
+     */
     @SuppressWarnings("ResultOfMethodCallIgnored")
     public static boolean isUrlValid(String url) {
         try {
@@ -18,6 +29,12 @@ public class UrlValidatorHelper {
         }
     }
 
+    /**
+     * Validates the provided URL string and throws a {@link CustomException} if it is invalid.
+     *
+     * @param url the URL string to validate
+     * @throws CustomException if the URL format is invalid
+     */
     public static void checkUrlValidThrowException(String url) throws CustomException {
         if (!isUrlValid(url))
             throw new CustomException("Invalid URL format");
