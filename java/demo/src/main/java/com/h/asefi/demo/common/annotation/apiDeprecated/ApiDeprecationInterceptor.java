1532de67f1b0cd746b7736c04a3223d6c6ff75a4
h.asefi <assefi.hossein@gmail.com>
2025-08-03 08:31:08 +0330

    remove unused code

--- java/demo/src/main/java/com/h/asefi/demo/common/annotation/apiDeprecated/ApiDeprecationInterceptor.java
+++ java/demo/src/main/java/com/h/asefi/demo/common/annotation/apiDeprecated/ApiDeprecationInterceptor.java
@@ -2,7 +2,6 @@ package com.h.asefi.demo.common.annotation.apiDeprecated;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
