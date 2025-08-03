635841be2d2c01ee2767adb3eb0c2d85a7a7bdc3
h.asefi <assefi.hossein@gmail.com>
2025-08-03 08:32:07 +0330

    update LoggingAspect javadoc

--- java/demo/src/main/java/com/h/asefi/demo/common/aop/LoggingAspect.java
+++ java/demo/src/main/java/com/h/asefi/demo/common/aop/LoggingAspect.java
@@ -13,11 +13,26 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
     private final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
+    /**
+     * Logs before entering a method, including the method signature and all
+     * arguments that were passed to it.
+     * 
+     * @param joinPoint The aspect's join point that contains information about the
+     *                  method.
+     */
     @Before("execution(* com.h.asefi.demo..*(..))")
     public void logBefore(JoinPoint joinPoint) {
         logger.info("Entering method: {} with arguments: {}", joinPoint.getSignature(), joinPoint.getArgs());
     }
 
+    /**
+     * Logs after exiting a method, including the method signature and the result it
+     * returned.
+     * 
+     * @param joinPoint The aspect's join point that contains information about the
+     *                  method.
+     * @param result    The result returned by the method.
+     */
     @AfterReturning(pointcut = "execution(* com.h.asefi.demo..*(..))", returning = "result")
     public void logAfter(JoinPoint joinPoint, Object result) {
         logger.info("Exiting method: {} with result: {}", joinPoint.getSignature(), result);
