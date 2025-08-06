28cd729f7b180bd924cfee36e404b77a76b1d416
h.asefi <assefi.hossein@gmail.com>
2025-08-06 08:06:01 +0330

    add SecurityHeadersFilter javadoc

--- java/demo/src/main/java/com/h/asefi/demo/common/filter/SecurityHeadersFilter.java
+++ java/demo/src/main/java/com/h/asefi/demo/common/filter/SecurityHeadersFilter.java
@@ -9,6 +9,23 @@ import java.io.IOException;
 
 @Component
 public class SecurityHeadersFilter extends OncePerRequestFilter {
+    /**
+     * Applies security headers to the HTTP response to enhance security.
+     * 
+     * This filter adds the following headers to the response:
+     * - X-XSS-Protection: Enables cross-site scripting (XSS) filter in browsers.
+     * - X-Content-Type-Options: Prevents MIME type sniffing.
+     * - Content-Security-Policy: Restricts resources to be loaded from the same
+     * origin.
+     * 
+     * @param request     the HTTP request
+     * @param response    the HTTP response with security headers added
+     * @param filterChain the filter chain for continuing the request
+     * @throws IOException                      if an I/O error occurs during
+     *                                          filtering
+     * @throws jakarta.servlet.ServletException if an error occurs during filtering
+     */
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws IOException, jakarta.servlet.ServletException {
