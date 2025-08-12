e8143d9a56a2f16f3a8834b18f33f40e006c958f
h.asefi <assefi.hossein@gmail.com>
2025-08-12 17:27:28 +0330

    update DeprecatedEndpointLoggerInterceptor

--- java/demo/src/main/java/com/h/asefi/demo/common/interceptor/DeprecatedEndpointLoggerInterceptor.java
+++ java/demo/src/main/java/com/h/asefi/demo/common/interceptor/DeprecatedEndpointLoggerInterceptor.java
@@ -13,15 +13,34 @@ public class DeprecatedEndpointLoggerInterceptor implements HandlerInterceptor {
     private static final String DEPRECATED_HEADER = "Deprecated";
     private static final String LOG_FILE_PATH = "deprecated_endpoints.log";
 
+    /**
+     * Checks if the request contains the "Deprecated" header and if so logs the
+     * request to a file. The header is expected to be "true" if the API is
+     * deprecated.
+     *
+     * @param request  the HTTP request
+     * @param response the HTTP response
+     * @param handler  the handler (or handler method) chosen to handle the request
+     * @return true to continue processing the request
+     */
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)  {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String deprecatedHeader = request.getHeader(DEPRECATED_HEADER);
 
-        if("true".equals(deprecatedHeader)) {
+        if ("true".equals(deprecatedHeader)) {
             logDeprecatedCall(request);
         }
         return true;
     }
+
+    /**
+     * Logs a deprecated API call to a file. The log entry includes the timestamp,
+     * HTTP method, request URI, and the User-Agent header identifying the caller.
+     * If writing to the log file fails, an error message is printed to the standard
+     * error.
+     *
+     * @param request the HTTP request containing information about the API call
+     */
 
     private void logDeprecatedCall(HttpServletRequest request) {
         String logEntry = String.format(
