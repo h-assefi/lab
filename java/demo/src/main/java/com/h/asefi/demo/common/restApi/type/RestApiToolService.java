d0e3dfafe0d09641291ef61f875272700739094e
h.asefi <assefi.hossein@gmail.com>
2025-08-27 13:48:39 +0330

    update RestApiToolService

--- java/demo/src/main/java/com/h/asefi/demo/common/restApi/type/RestApiToolService.java
+++ java/demo/src/main/java/com/h/asefi/demo/common/restApi/type/RestApiToolService.java
@@ -1,5 +1,18 @@
 package com.h.asefi.demo.common.restApi.type;
 
+/**
+ * Enum representing the available HTTP client implementations for REST API
+ * operations.
+ * <p>
+ * Use this enum to select which underlying HTTP client should be used by the
+ * {@code RestApiServiceImpl}:
+ * <ul>
+ * <li>{@link #HttpClient} - Uses Java's built-in HttpClient
+ * (java.net.http.HttpClient).</li>
+ * <li>{@link #RestTemplate} - Uses Spring's RestTemplate.</li>
+ * </ul>
+ * This allows switching between different HTTP client strategies at runtime.
+ */
 public enum RestApiToolService {
     HttpClient,
     RestTemplate
